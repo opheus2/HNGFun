fd0156cfbb7d27a86b8f958c0592a5a9e84fd6dc
opheus2 <31169044+opheus2@users.noreply.github.com>
2018-04-14 17:30:57 -0400

    Update opheus.php

--- profiles/opheus.php
+++ profiles/opheus.php
@@ -1,4 +1,30 @@
+<?php
+// Create connection
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
+$sql = "SELECT intern_id, name, username, image_filename FROM interns_data_ WHERE username='opheus' ";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+		$name = $row["name"];
+		$username = $row["username"];
+		$imagelink = $row["image_filename"];
+    }
+} else {
+    echo "NO USER FOUND";
+}
+
+
+
+mysqli_close($conn);
+
+?> 
 <html>
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -45,9 +71,9 @@ button:hover, a:hover {
 <h2 style="text-align:center">User Profile</h2>
 
 <div class="card">
-  <img src="http://res.cloudinary.com/opheus/image/upload/v1523622319/IMG_20180404_091302_600.jpg" alt="ima" style="width:100%">
-  <h1>Ominiabohs Efemena David</h1>
-  <h2>@opheus</h2>
+  <img src="<?php echo $imagelink; ?>" alt="ima" style="width:100%">
+  <h1><?php echo $name; ?></h1>
+  <h2>@<?php echo $username; ?></h2>
   <p class="title">Web Designer & Developer, UI/UX Designer</p>
   <p>Delta State Univeristy (B.Sc Physics)</p>
   <p>Nigeria</p>
