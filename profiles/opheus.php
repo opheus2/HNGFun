30e5c19190449d8c78a9f7a51f7676c875dc3a6a
opheus2 <31169044+opheus2@users.noreply.github.com>
2018-04-14 06:52:13 -0400

    Update opheus.php

--- profiles/opheus.php
+++ profiles/opheus.php
@@ -1,3 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "\$tori58622685";
+$dbname = "hngfun";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT secret_word FROM secret_word";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+		echo $row["secret_word"];
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+
+
+
+?> 
+
 <!DOCTYPE html>
 <html>
 <head>
